a884d55b21bed72b59d30f443dcf605f595fcf5c
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-04-18 22:28:49 -0500

    images types

--- pokemon/src/components/cardPokemon.jsx
+++ pokemon/src/components/cardPokemon.jsx
@@ -5,10 +5,7 @@ const CardPokemon = ({pokemon}) => {
 
   return (
     <div className={styles.cardContent}>
-      <header className={styles.headerCard}>
-        <img className={styles.iconPokemon} />
-        <h1 className={styles.namePokemon}>{pokemon.name}</h1>
-      </header>
+      <h1 className={styles.namePokemon}>{pokemon.name}</h1>
       <img className={styles.imagePokemon} src={pokemon.sprites.other.dream_world.front_default} />
       <ul>
         {
